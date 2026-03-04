567c5536d4c6b40172c1ac3facb2a843d215b9f2
francoisgosselin <52206358+francoisgosselin@users.noreply.github.com>
2026-03-04 13:58:28 -0500

    Update server.py

--- lab_udp/server.py
+++ lab_udp/server.py
@@ -11,6 +11,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     print(f"Serveur UDP sur {HOST}:{PORT}")
     while True:
         data, addr = s.recvfrom(65535)
+        #Pourquoi utiliser le nombre 65535 ici? Éviter les chiffres magiques dans le code.
         print(f"Recu {len(data)} octets de {addr}")
 
         body, hash_recu = data.rsplit(b"\x00", 1)
@@ -18,10 +19,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         message = body[NONCE_SIZE:]
 
         calc = hashlib.sha256(nonce + message).digest()
-
+        #nonce + message = body. Pas nécessaire de recombiner ces deux variables ici.
+        
         if calc == hash_recu:
             print(f"Hash valide  | Message : {message.decode('utf-8', errors='replace')}")
             s.sendto(b"Message et hachage valides", addr)
         else:
             print("Hash invalide message corrompu ou modifie")
+
             s.sendto(b"Erreur de hachage", addr)
